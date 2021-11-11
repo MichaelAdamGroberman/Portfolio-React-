0944dbf037ee8f817afdf788e85e11f57ed5b2f0
Michael Adam Groberman <michael.groberman@me.com>
2021-11-10 19:03:18 -0500

    uploaded sharedElements component
    defining CSS styles in js

---
+++ src/components/sharedElements.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+
+export const Sectioncontainer = styled.section`
+width:100%;
+display:flex;
+background-color:#333333 ;
+`
+export const SectionTitle = styled.h1`
+padding-right:40px ;
+padding-left:20px ;
+font-size: 60px;
+width:20%;
+
+color:#1C6EA4;
+`
+export const SectionContent = styled.section`
+margin-top: 5px;
+border-left: 5px solid #1C6EA4;
+padding-bottom:5px;
+margin-bottom: 25px;
+width:80%;
+
+`
+export const SectionText = styled.p`
+align-self: center;
+padding-left: 20px;
+padding-right: 60px;
+color:#1C6EA4;
+`
