609ca836d0e9af5bbbe3d5f3f9e35dc1b1adf253
Michael Adam Groberman <michael.groberman@me.com>
2021-11-11 15:56:25 -0500

    updated nav
    added a link to my resume, added nav link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,47 @@
 import React from 'react'
-import { NavContainer, NavTop, NavTitle, NavLInks, NavLink, NavBottom, NavDesc, } from './navElements'
+import { NavContainer, NavTop, NavTitle, NavLInks, NavLinka, NavLink, NavBottom, NavDesc, } from './navElements'
+// importing styled components
+import PDF from '../../assets/files/MichaelGroberman_Resume.pdf';
+
 const Nav = () => {
     return (
         <NavContainer>
             <NavTop>
                 <NavTitle>Micheal Groberman</NavTitle>
                 <NavLInks>
-                    <NavLink to=''>About Me</NavLink>
-                    <NavLink to=''>Work</NavLink>
-                    <NavLink to=''>Contact Me</NavLink>
-                    <NavLink to=''>Resume</NavLink>
+
+                    <NavLink
+                    
+                        activeClass="active"
+                        to="About"
+                        // targets component with id of "about"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}>
+                        About Me
+                    </NavLink>
+
+                    <NavLink
+                        activeClass="active"
+                        to="Work"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}>
+                        Work
+                    </NavLink>
+
+                    <NavLink
+                        activeClass="active"
+                        to="Contact"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}>
+                        Contact Me
+                    </NavLink>
+                    <NavLinka href={PDF} target="_blank">Resume</NavLinka>
 
                 </NavLInks>
             </NavTop>
