5126e8896b9a4ac5d83914ea632d98017c156243
Michael Adam Groberman <michael.groberman@me.com>
2021-11-11 15:55:06 -0500

    updated index.js
    added div section id's and comments, added some projects and images

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -6,51 +6,53 @@ import horiseon from '../../assets/images/horiseon.png'
 import PGenerator from '../../assets/images/PasswordGenerator.png'
 import placeholder from '../../assets/images/placeholder.png'
 import weather from '../../assets/images/WeatherDash.png'
+// importing images 
 const Work = () => {
 
     return (
-        <Sectioncontainer>
+        <Sectioncontainer id="Work">
             <SectionTitle>Work</SectionTitle>
             <SectionContent>
                 <ProjectsContainer>
                     <ProjectCard background={vegitaite}>
+                        {/* passing in img as prop to component */}
                         <ProjectDesc >
 
-                            <ProjectHeading>test</ProjectHeading>
-                            <ProjectText>test</ProjectText>
-                            <ProjectLink href="">test</ProjectLink>
+                            <ProjectHeading>Vegitate</ProjectHeading>
+                            <ProjectText> A movie trailer and preview webapp</ProjectText>
+                            <ProjectLink href="https://stephenwong-git.github.io/Vegitate/" target="_blank">Go to project</ProjectLink>
 
                         </ProjectDesc>
                     </ProjectCard>
                     <ProjectCard background={horiseon}>
                         <ProjectDesc >
-                            <ProjectHeading>test</ProjectHeading>
-                            <ProjectText>test</ProjectText>
-                            <ProjectLink href="">test</ProjectLink>
+                            <ProjectHeading>Horiseon </ProjectHeading>
+                            <ProjectText>Horiseon, a company specializing in online marketing would like their website to be optimized, and for some of the code to be fixed.</ProjectText>
+                            <ProjectLink href="https://github.com/MichaelAdamGroberman/Horiseon-Code-Refactoring" target="_blank">Go to project</ProjectLink>
 
                         </ProjectDesc>
                     </ProjectCard>
                     <ProjectCard background={PGenerator}>
                         <ProjectDesc >
-                            <ProjectHeading>test</ProjectHeading>
-                            <ProjectText>test</ProjectText>
-                            <ProjectLink href="">test</ProjectLink>
+                            <ProjectHeading>Secure Password Generator</ProjectHeading>
+                            <ProjectText>A password is randomly generated based on set criteria that the user provides when prompted via js alerts/prompts.</ProjectText>
+                            <ProjectLink href="https://github.com/MichaelAdamGroberman/Password-Generator" target="_blank">Go to project</ProjectLink>
 
                         </ProjectDesc>
                     </ProjectCard>
                     <ProjectCard background={weather}>
                         <ProjectDesc >
-                            <ProjectHeading>test</ProjectHeading>
-                            <ProjectText>test</ProjectText>
-                            <ProjectLink href="">test</ProjectLink>
+                            <ProjectHeading>Weather-Dashboard</ProjectHeading>
+                            <ProjectText>Our assignment was to create a weather dashboard that leverage a 3rd party server api to propigate our web application with relevant data.</ProjectText>
+                            <ProjectLink href="" target="_blank">Go to project</ProjectLink>
 
                         </ProjectDesc>
                     </ProjectCard>
                     <ProjectCard background={placeholder}>
                         <ProjectDesc >
-                            <ProjectHeading>test</ProjectHeading>
-                            <ProjectText>test</ProjectText>
-                            <ProjectLink href="">test</ProjectLink>
+                            <ProjectHeading>Placeholder</ProjectHeading>
+                            <ProjectText>Placeholder</ProjectText>
+                            <ProjectLink href="" target="_blank">Go to project</ProjectLink>
 
                         </ProjectDesc>
                     </ProjectCard>
