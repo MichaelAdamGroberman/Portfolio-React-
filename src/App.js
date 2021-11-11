53368a1ca8212289cf54d0abff015dd0feb68522
Michael Adam Groberman <michael.groberman@me.com>
2021-11-11 16:00:03 -0500

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+// import components to main view
 import About from './components/About';
+import Contact from './components/Contact';
 import Nav from './components/Nav';
 import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
+      {/* add components */}
       <Nav />
       <About />
       <Work />
+      <Contact />
     </div>
   );
 }
